55f7bc9217698c454499394609be989424857cc7
VItor Justamand <justamandd@gmail.com>
2023-03-16 18:38:57 -0300

    Var and Val difference

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
+    //var can be reassigned
+    var car: String = "Fiat Uno"
+    car = "Fiat Palio"
+
+    //val cannot be reassigned, is like a constant
+    val birthDate: String = "19/11/2003"
+
     println("Hello, World!")
 }
